233b543bff0929fd33d05756072b5f9d11c623e5
Paul Salaberria <paulsmsm@Pauls-iMac.local>
2015-10-28 16:14:20 +0100

    Update http dependency and project name

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject clj-oauth2 "0.5.0"
+(defproject psalaberria002/clj-oauth2 "0.5.0"
   :description "clj-http and ring middlewares for OAuth 2.0"
+  :url "https://github.com/psalaberria002/clj-oauth2"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.5.0"]
-                 [clj-http "0.3.2"]
+                 [clj-http "1.1.0"]
                  [uri "1.1.0"]
                  [commons-codec/commons-codec "1.6"]
                  [ring "1.4.0"]]
